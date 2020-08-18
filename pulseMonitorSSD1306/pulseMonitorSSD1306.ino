79a58a0f369e61082c16ad35895261e0a2ee5f05
mymaestro <mymaestro@gmail.com>
2020-08-18 15:46:19 -0500

    Clean, change status bar

--- pulseMonitorSSD1306/pulseMonitorSSD1306.ino
+++ pulseMonitorSSD1306/pulseMonitorSSD1306.ino
@@ -31,7 +31,6 @@ Warren Gill: Changed the code to use Timer1 for Arduino Leonardo and Arduino Mic
 */
 #define USE_ARDUINO_INTERRUPTS true
 #include <PulseSensorPlayground.h>
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -40,8 +39,8 @@ Warren Gill: Changed the code to use Timer1 for Arduino Leonardo and Arduino Mic
 
 /*********************
 * Pin assignments:
-  Pins going to the PCD8544 routed through a 4050 level shifter
-  Arduino___  Device
+  Pins going to the SSD1306 128x32 OLED
+  Arduino Micro Device
   A0          Pulse sensor
   D2          Pin 1 on OLED: (SDA)
   D3          Pin 2 on OLED: (SCL) 
@@ -50,7 +49,6 @@ Warren Gill: Changed the code to use Timer1 for Arduino Leonardo and Arduino Mic
   D11         LED through a resistor, for fading heartbeat
   D13         LED heartbeat detected
 */
-
 /*
    The format of our output.
 
@@ -67,7 +65,7 @@ const int OUTPUT_TYPE = SERIAL_PLOTTER;
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Pins and variables
@@ -75,39 +73,36 @@ const int buttonPin = 5;    // A button on this pin controls the backlightPin
 const int blinkPin = 13;    // Blinks an LED at each beat
 const int fadePin = 11;     // Do fancy classy fading blink at each beat
 const int backlightPin = 8; // Control the display backlight through a 2222 transistor
-int       pulsePin = 0;     // Pulse Sensor purple wire connected to analog pin 0
-int       fadeRate = 0;     // used to fade LED on with PWM on fadePin
 
-Bounce buttonPress = Bounce(buttonPin,10);
+Bounce buttonPress = Bounce(buttonPin, 10);
 
-int _circularBuffer[SCREEN_WIDTH];  //fast way to store values (rather than an ArrayList with remove calls)
-int _curWriteIndex = 0;             // tracks where we are in the circular buffer
+int _circularBuffer[SCREEN_WIDTH]; //fast way to store values (rather than an ArrayList with remove calls)
+int _curWriteIndex = 0;            // tracks where we are in the circular buffer
 
 // for tracking fps
 unsigned long _totalFrameCount = 0;
 unsigned long _startTimeStamp = 0;
 
 // status bar
-boolean      _drawStatusBar = true; // change to show/hide status bar
-int          _graphHeight = SCREEN_HEIGHT;
+boolean _drawStatusBar = true; // change to show/hide status bar
+int _graphHeight = SCREEN_HEIGHT;
+
 /*
-   Pinout:
-     PULSE_INPUT = Analog Input. Connected to the pulse sensor
-      purple (signal) wire.
-     PULSE_BLINK = digital Output. Connected to an LED (and 220 ohm resistor)
-      that will flash on each detected pulse.
-     PULSE_FADE = digital Output. PWM pin onnected to an LED (and resistor)
-      that will smoothly fade with each pulse.
-      NOTE: PULSE_FADE must be a pin that supports PWM. Do not use
-      pin 9 or 10, because those pins' PWM interferes with the sample timer.
+Pinout:
+ PULSE_INPUT = Analog Input. Connected to the pulse sensor purple (signal) wire.
+ PULSE_BLINK = digital Output. Connected to an LED (and 220 ohm resistor) that will flash on each detected pulse.
+ PULSE_FADE = digital Output. PWM pin onnected to an LED (and resistor) that will smoothly fade with each pulse.
+ 
+ NOTE: PULSE_FADE must be a pin that supports PWM. Do not use pin 9 or 10, because those pins' PWM interferes with the sample timer.
 */
-const int PULSE_INPUT = A0;
-const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
-const int PULSE_FADE = 5;
+
+const int PULSE_INPUT      = A0;
+const int PULSE_BLINK      = 13;   // Pin 13 is the on-board LED
+const int PULSE_FADE       = 5;
 const int MIN_ANALOG_INPUT = 0;
 const int MAX_ANALOG_INPUT = 1023;
-const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle
-const int DELAY_LOOP_MS = 5; // change to slow down how often to read and graph value
+const int THRESHOLD        = 550;  // Adjust this number to avoid noise when idle
+const int DELAY_LOOP_MS    = 5;    // change to slow down how often to read and graph value
 
 /*
    All the PulseSensor Playground functions.
@@ -115,35 +110,35 @@ const int DELAY_LOOP_MS = 5; // change to slow down how often to read and graph 
 PulseSensorPlayground pulseSensor;
 
 void setup()
-{ 
-  pinMode(blinkPin, OUTPUT);        // set the LED pins as outputs
+{
+  pinMode(blinkPin, OUTPUT); // set the LED pins as outputs
   pinMode(fadePin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(backlightPin, OUTPUT);
-  
-  Serial.begin(115200);             // we agree to talk fast!
+
+  Serial.begin(115200); // we agree to talk fast!
 
   // Configure the PulseSensor manager.
 
   pulseSensor.analogInput(PULSE_INPUT);
   pulseSensor.blinkOnPulse(PULSE_BLINK);
   pulseSensor.fadeOnPulse(PULSE_FADE);
-
   pulseSensor.setSerial(Serial);
   pulseSensor.setOutputType(OUTPUT_TYPE);
   pulseSensor.setThreshold(THRESHOLD);
 
   // Now that everything is ready, start reading the PulseSensor signal.
-  if (!pulseSensor.begin()) {
+  if (!pulseSensor.begin())
+  {
     /*
-       PulseSensor initialization failed,
-       likely because our particular Arduino platform interrupts
-       aren't supported yet.
+       PulseSensor initialization failed, likely because our particular Arduino
+       platform interrupts aren't supported yet.
 
        If your Sketch hangs here, try PulseSensor_BPM_Alternative.ino,
        which doesn't use interrupts.
     */
-    for(;;) {
+    for (;;)
+    {
       // Flash the led to show things didn't work.
       digitalWrite(PULSE_BLINK, LOW);
       delay(50);
@@ -151,20 +146,19 @@ void setup()
       delay(50);
     }
   }
-  
+
   // Initialize the OLED
-  
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64, 0x3C for 128x32
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64, 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
-  
+
   display.display();
   delay(2000);
-  // Clear the buffer
   display.clearDisplay();
-
   display.setTextSize(1);
   display.setTextColor(WHITE, BLACK);
   display.setCursor(0, 0);
@@ -173,7 +167,8 @@ void setup()
   delay(500);
   display.clearDisplay();
 
-  if(_drawStatusBar){
+  if (_drawStatusBar)
+  {
     _graphHeight = SCREEN_HEIGHT - 10;
   }
 }
@@ -181,78 +176,71 @@ void setup()
 void loop()
 {
   /*
-     Wait a bit.
-     We don't output every sample, because our baud rate
-     won't support that much I/O.
+     Wait a bit. We don't output every sample, because our baud rate won't support that much I/O.
   */
   delay(DELAY_LOOP_MS); // change to slow down how often to read and graph value
 
-  if(_startTimeStamp == 0){
+  if (_startTimeStamp == 0)
+  {
     _startTimeStamp = millis();
   }
-  
+
   int analogVal = pulseSensor.getLatestSample();
   pulseSensor.outputSample();
-  
+
   _circularBuffer[_curWriteIndex++] = analogVal;
-      
-  if(_curWriteIndex >= display.width()){
+
+  if (_curWriteIndex >= display.width())
+  {
     _curWriteIndex = 0;
   }
-  
+
   int myBPM = pulseSensor.getBeatsPerMinute(); // Calls pulseSensor object to get beats/min
-  
-  /*
-     If a beat has happened since we last checked,
-     write the per-beat information to Serial.
-   */
-  if (pulseSensor.sawStartOfBeat()) {
+
+  // If a beat has happened since we last checked, write the per-beat information to Serial.
+  if (pulseSensor.sawStartOfBeat())
+  {
     pulseSensor.outputBeat();
   }
-  
+
   display.clearDisplay();
-  if(_drawStatusBar){
-    // Draw current val
-    display.setCursor(0, 0);
+  if (_drawStatusBar)
+  {
+    // Draw current beats per minute
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.getTextBounds("XXS BPM", 0, 0, &x1, &y1, &w, &h);
+    display.setCursor(display.width() - w, 0);
     display.print(myBPM);
     display.print(" BPM");
-  
-    if(_totalFrameCount > 0){
-      int16_t  x1, y1;
-      uint16_t w, h;
-      unsigned long elapsedTime = millis() - _startTimeStamp;
-      float fps = _totalFrameCount / (elapsedTime / 1000.0);
-      display.getTextBounds("XX.XX fps", 0, 0, &x1, &y1, &w, &h);
-      display.setCursor(display.width() - w, 0);
-      display.print(fps);
-      display.print(" fps");
-    }
   }
 
   // Draw the line graph
-  int xPos = 0; 
-  for (int i = _curWriteIndex; i < display.width(); i++){
-    // draw the line
+  int xPos = 0;
+  for (int i = _curWriteIndex; i < display.width(); i++)
+  {
+    // Draw the line
     int analogVal = _circularBuffer[i];
     drawLine(xPos, analogVal);
     xPos++;
   }
-  
-  for(int i = 0; i < _curWriteIndex; i++){
-    //draw the line
+
+  for (int i = 0; i < _curWriteIndex; i++)
+  {
+    // Draw the line
     int analogVal = _circularBuffer[i];
     drawLine(xPos, analogVal);
-    xPos++;;
+    xPos++;
+    ;
   }
-  
+
   display.display();
   _totalFrameCount++;
   delay(DELAY_LOOP_MS);
-
 }
 
-
-void drawLine(int xPos, int analogVal){
+void drawLine(int xPos, int analogVal)
+{
   int lineHeight = map(analogVal, MIN_ANALOG_INPUT, MAX_ANALOG_INPUT, 0, _graphHeight);
   int yPos = display.height() - lineHeight;
   display.drawFastVLine(xPos, yPos, lineHeight, SSD1306_WHITE);
